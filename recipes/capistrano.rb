38c67917adde0209051ce77bf15cc3d181f92898
Karsten Silkenbäumer <ksi@kluks.de>
2012-11-29 01:58:23 +0100

    Fix wrong usage of config wizard

--- recipes/capistrano.rb
+++ recipes/capistrano.rb
@@ -1,18 +1,17 @@
+say_wizard "Configuring Capistrano"
+prefs[:capistrano] = multiple_choice "Capistrano deployment?", [["Virtual Server","vserver"]] unless prefs.has_key? :capistrano
+if prefer :capistrano, "vserver"
+  prefs[:deploy_server] = ask_wizard "Servername to deploy to (omit www as subdomain):" unless prefs.has_key? :deploy_server
+  prefs[:deploy_user]   = ask_wizard "Username to use for deployment:" unless prefs.has_key? :deploy_user
+  prefs[:deploy_to]     = "/var/www/vhosts/#{prefs[:deploy_server]}/rails/#{app_name}" unless prefs.has_key? :deploy_to
+end
+
 before_config do
   # Code here is run before any configuration prompts.
-  prefs[:capistrano] = multiple_choice "Capistrano deployment?", [["Virtual Server","vserver"]] unless prefs.has_key? :capistrano
-  if prefer :capistrano, "vserver"
-    prefs[:deploy_server] = ask_wizard "Servername to deploy to (omit www as subdomain):" unless prefs.has_key? :deploy_server
-    prefs[:deploy_user]   = ask_wizard "Username to use for deployment:" unless prefs.has_key? :deploy_user
-    prefs[:deploy_repo]   = `git remote -v | perl -nE 'm/^\w+\s+(\S+)\s+\(fetch\)/ && print $!'` unless prefs.has_key? :deploy_repo
-    prefs[:deploy_repo]   = ask_wizard "Repository to fetch from:" unless prefs.has_key? :deploy_repo
-    prefs[:deploy_to]     = "/var/www/vhosts/#{prefs[:deploy_server]}/rails/#{app_name}" unless prefs.has_key? :deploy_to
-  end
 end
 
 # add gems here
 gem 'capistrano'
-
 
 after_bundler do
   repo = 'https://raw.github.com/kassi/rails-apps-composer-recipes/master/templates/capistrano'
@@ -20,16 +19,16 @@ after_bundler do
   # Use this section to run generators and rake tasks.
   # Download any files from a repository for models, controllers, views, and routes.
   run "capify ."
-  gsub_file 'Capfile', /\s*# load 'deploy\/assets'/, "load 'deploy/assets'"
+  gsub_file 'Capfile', /^\s*# load 'deploy\/assets'/m, "load 'deploy/assets'"
 
   remove_file 'config/deploy.rb'
-  create_file 'config/deploy.rb' do <<-DEPLOY
+  create_file 'config/deploy.rb' do <<-DEPLOY.gsub /^    /, ""
     require "bundler/capistrano"
 
     set :bundle_flags, "--deployment --binstubs"
 
     set :application, "#{app_name}"
-    set :repository,  "#{prefs[:deploy_repo]}"
+    set :repository,  "ssh://"
     # set :branch, 'master'
 
     set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
@@ -68,22 +67,30 @@ DEPLOY
   end
 
   copy_from_repo 'config/deploy/capistrano_database.rb', :repo => repo
-  if prefer :mysql or prefer :database, 'mysql'
+  if prefer :mysql, true or prefer :database, 'mysql'
     copy_from_repo 'config/deploy/database.yml.erb', :repo => repo
-    inject_into_file 'config/deploy.rb', 'require "./config/deploy/capistrano_database"', :after => 'require "bundler/capistrano"'
+    inject_into_file 'config/deploy.rb', :after => 'require "bundler/capistrano"' do <<-RUBY
+
+require "./config/deploy/capistrano_database"
+RUBY
+    end
   end
 
   # add some server settings
   if prefer :authentication, 'devise'
-    gsub_file 'config/initializers/devise.rb', /please-change-me-at-config-initializers-devise@example.com/, "webservice@#{prefs['deploy_server']}"
+    gsub_file 'config/initializers/devise.rb', /please-change-me-at-config-initializers-devise@example.com/, "webservice@#{prefs[:deploy_server]}"
   end
-  gsub_file 'config/environment/production.rb', /config.action_mailer.delivery_method = :smtp/, "config.action_mailer.delivery_method = :sendmail"
+  gsub_file 'config/environments/production.rb', /config.action_mailer.delivery_method = :smtp/, "config.action_mailer.delivery_method = :sendmail"
+  gsub_file 'config/environments/production.rb', /config.action_mailer.default_url_options = .*/, "config.action_mailer.default_url_options = { :host => '#{prefs[:deploy_server]}' }"
 end
 
 after_everything do
   # This block is run after the 'after_bundler' block.
   # Use this section to finalize the application.
   # Run database migrations or make a final git commit.
+  prefs[:deploy_repo] = `git remote -v | perl -nE 'm/^\\w+\\s+(\\S+)\\s+\\(fetch\\)/ && print $1'` unless prefs.has_key? :deploy_repo
+  prefs[:deploy_repo] = ask_wizard "Repository to fetch from:" if prefs[:deploy_repo].empty?
+  inject_into_file 'config/deploy.rb', prefs[:deploy_repo], :after => 'ssh://'
 
   ### GIT ###
   if prefer :git, true
